d306bd9b3a2974b3119d5f6ffbb450bf8a5a8ae7
Don Lazzylife <88842775+lazzylife42@users.noreply.github.com>
2023-03-28 18:39:38 +0200

    next gigs

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sab <sab@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: smonte-e <smonte-e@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 18:49:53 by sab               #+#    #+#             */
-/*   Updated: 2023/03/27 18:53:09 by sab              ###   ########.fr       */
+/*   Updated: 2023/03/28 17:15:59 by smonte-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-char	*gqet_next_line(int fd);
+char	*qet_next_line(int fd);
 
 #endif
