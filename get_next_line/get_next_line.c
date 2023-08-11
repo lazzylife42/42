9209122151d0738489d185254fb89ed3a57177b0
Don Lazzylife <88842775+lazzylife42@users.noreply.github.com>
2023-08-11 16:45:26 +0200

    ca marche mieux, mais tjr cassé

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: smonte-e <smonte-e@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 19:18:42 by smonte-e          #+#    #+#             */
-/*   Updated: 2023/08/11 14:54:50 by smonte-e         ###   ########.fr       */
+/*   Updated: 2023/08/11 16:38:30 by smonte-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,53 +18,38 @@ char *get_next_line(int fd)
 	int newline_index;
 	static char *buff;
 	char *line;
-/*
+
 	if ((fd < 0) || (BUFFER_SIZE <= 0))
 		return (NULL);
-	bytes_readed = 1;
-	if (!buff)
-	{
-		buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		if (!buff)
-			return (NULL);
-	}
-	while (bytes_readed > 0)
-	{
-		bytes_readed = read(fd, buff, BUFFER_SIZE);
-		if (bytes_readed == -1)
-			free(buff);
-		newline_index = find_newline_index(buff);
-		if (newline_index >= 0)
-		{
-			line = copy_line(buff, line, newline_index);
-			if (!line)
-				break;
-			return (line);
-		}
-	}
-//	free(buff);
-
-*/
-
-	if (!buff)
-		buff = gnl_read(fd, buff);
+	buff = gnl_read(fd, buff);
+	if (!buff || buff == NULL)
+		return (NULL);
 	newline_index = find_newline_index(buff);
 	if (newline_index < 0)
+	{
+		free(buff);
 		return (NULL);
-
-	line = copy_line(buff, line, newline_index);
-
-	return (line);
+	}
+	line = malloc(sizeof(char) * (newline_index + 1));
+	if (!line)
+	{
+		free(buff);
+		free(line);
+		return (NULL);
+	}
+	return (copy_line(buff, line, newline_index));
 }
 
 char *copy_line(char *buff, char *line, int newline_index)
 {
+/*
 	line = malloc(sizeof(char) * (newline_index + 1));
 	if (!line)
 	{
 		free(line);
 		return (NULL);
 	}
+*/
 	ft_strlcpy(line, buff, newline_index + 1);
 	ft_strlcpy(buff, &buff[newline_index + 1], ft_strlen(buff) - newline_index);
 	return (line);
@@ -74,11 +59,12 @@ char *gnl_read(int fd, char *buff)
 {
 	int bytes_readed;
 	
-	if ((fd < 0) || (BUFFER_SIZE <= 0))
-		return (NULL);
 	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buff)
+	{
+		free(buff);
 		return (NULL);
+	}
 	bytes_readed = 1;
 	while (bytes_readed > 0)
 	{
