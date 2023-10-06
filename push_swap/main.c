ca5be2680401bf67216e3cfeb8b9e094c2509671
Sabino Monte Engombe <smonte-e@c1r6s3.42lausanne.ch>
2023-10-06 14:47:33 +0200

    push_swap cassé a 5 mais projet rangé

---
+++ push_swap/main.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smonte-e <smonte-e@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 17:43:42 by smonte-e          #+#    #+#             */
+/*   Updated: 2023/10/06 14:42:45 by smonte-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_stack	*a;
+	t_stack	*b;
+
+	a = NULL;
+	b = NULL;
+	if (argc == 1|| (argc == 2 && !argv[1][0]))
+		return (1);		
+	stack_init(&a, argv);
+	if (!stack_sorted(a))
+	{
+		if (ft_llen(a) == 2)
+			sa(&a);
+		else if (ft_llen(a) == 3)
+			tiny_sort(&a);
+		else
+		{
+			radix(&a, &b);
+/*			if (!stack_sorted(a))
+			{
+				while (!stack_sorted(a))
+					rra(&a);
+			}
+*/		}
+	}
+	free_stack(&b);
+	free_stack(&a);
+}
---
+++ push_swap/push_swap.h
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smonte-e <smonte-e@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 14:50:51 by sab               #+#    #+#             */
+/*   Updated: 2023/10/06 14:24:54 by smonte-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include "../libft/libft.h"
+
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+#define TRUE 	1
+#define FALSE	0
+
+typedef struct t_cell
+{
+	int		data;
+	struct t_cell *next;
+	struct t_cell *prev;
+}t_stack;
+
+/*	ERROR, INIT AND FREE	*/
+
+void	stack_init(t_stack **a, char **argv);
+void 	free_stack(t_stack **node);
+
+/*	LINKED LIST				*/
+
+void	ft_lprint(t_stack *node);
+int		ft_llen(t_stack* node);
+int		ft_lpush(t_stack** head, int new_data);
+int		ft_lpush_back(t_stack** head, int new_data);
+int		ft_ldelete(t_stack** head, t_stack* del);
+
+/*	ALGORYTHME				*/
+
+t_stack	*find_highest(t_stack *node);
+t_stack *find_lowest(t_stack *node);
+int		stack_sorted(t_stack *stack);
+void	tiny_sort(t_stack **a);
+void	five_sort(t_stack **a, t_stack **b);
+void	radix(t_stack **a, t_stack **b);
+
+/*	COMMANDS				*/
+
+void	sa(t_stack **a);
+void 	sb(t_stack **b);
+void	ss(t_stack **a, t_stack **b);
+void	ra(t_stack **a);
+void	rb(t_stack **b);
+void	rr(t_stack **a, t_stack **b);
+void	rra(t_stack **a);
+void	rrb(t_stack **b);
+void	rrr(t_stack **a, t_stack **b);
+void	pa(t_stack **a, t_stack **b);
+void	pb(t_stack **a, t_stack **b);
+
+#endif
