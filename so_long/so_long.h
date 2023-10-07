83650d8c1483ff223102853762efe90112aca163
Don Lazzylife <88842775+lazzylife42@users.noreply.github.com>
2023-10-07 15:46:07 +0200

    first so_long

---
+++ so_long/so_long.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smonte-e <smonte-e@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/07 15:25:20 by smonte-e          #+#    #+#             */
+/*   Updated: 2023/10/07 15:39:40 by smonte-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include "libft/libft.h"
+# include "minilibx/mlx.h"
+
+#endif
