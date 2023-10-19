f884538fc948ee9dc5a6c17201418eb497bbb066
Don Lazzylife <88842775+lazzylife42@users.noreply.github.com>
2023-10-20 01:53:00 +0200

    bon debut

--- so_long/src/main.c
+++ so_long/src/main.c
@@ -6,11 +6,12 @@
 /*   By: smonte-e <smonte-e@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 15:38:22 by smonte-e          #+#    #+#             */
-/*   Updated: 2023/10/19 22:18:36 by smonte-e         ###   ########.fr       */
+/*   Updated: 2023/10/20 01:30:51 by smonte-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
+#include <fcntl.h>
  
 int on_destroy(t_data *data)
 {
@@ -35,12 +36,24 @@ int main(void)
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
 		return (1);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, 896, 320, "hi :)");
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 900, 600, "Les Singes Viennent de Sortir du Zoo");
 	if (!data.win_ptr)
 		return (free(data.mlx_ptr), 1);
-		
-	map_to_str(data);
 	
+	
+
+	data.textures[0] = mlx_xpm_file_to_image(data.mlx_ptr, "sprites/xpm/kaaris.xpm", &data.width, &data.height);
+	data.textures[1] = mlx_xpm_file_to_image(data.mlx_ptr, "sprites/xpm/harambe.xpm", &data.width, &data.height);
+    data.textures[2] = mlx_xpm_file_to_image(data.mlx_ptr, "sprites/xpm/tile01.xpm", &data.width, &data.height);
+    data.textures[3] = mlx_xpm_file_to_image(data.mlx_ptr, "sprites/xpm/tile02.xpm", &data.width, &data.height);
+    data.textures[4] = mlx_xpm_file_to_image(data.mlx_ptr, "sprites/xpm/tile03.xpm", &data.width, &data.height);
+	map_to_str(data);
+/*	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[0], 0, 0);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[1], 64, 0);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[2], 128, 0);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[3], 192, 0);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[4], 256, 0);
+*/	
 	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
 	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
 	mlx_loop(data.mlx_ptr);
--- so_long/src/map.c
+++ so_long/src/map.c
@@ -6,7 +6,7 @@
 /*   By: smonte-e <smonte-e@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 20:47:56 by smonte-e          #+#    #+#             */
-/*   Updated: 2023/10/19 23:17:20 by smonte-e         ###   ########.fr       */
+/*   Updated: 2023/10/20 01:52:35 by smonte-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ void map_to_str(t_data data)
 	fd = open("map/level1.ber", O_RDONLY);
 	while ((str = get_next_line(fd)))
 	{
-		ft_printf("map: %s\n", str);
-//		char_to_sprite(data, str);
+		ft_printf("map: %s", str);
+		char_to_sprite(data, str);
 		free(str);
 	}
 	close(fd);
@@ -31,64 +31,48 @@ void map_to_str(t_data data)
 
 void sprite_init(t_data data)
 {
-	int i;
-	char *texture_files[] = {
-	"../sprites/xpm/kaaris.xpm",
-	"../sprites/xpm/harambe.xpm",
-	"../sprites/xpm/tile01.xpm",
-	"../sprites/xpm/tile02.xpm",
-	"../sprites/xpm/tile03.xpm"
-	};
-	
-	i = 0;
-	data.width = 64;
-	data.height = 64;
-	while (i < 5) 
-	{
-		data.textures[i] = mlx_xpm_file_to_image(data.mlx_ptr, texture_files[i], &data.width, &data.height);
-		data.addr[i] = mlx_get_data_addr(data.textures[i], &data.bits_per_pixel, &data.line_length, &data.endian);
-		i++;
-	}
+	data.textures[0] = mlx_xpm_file_to_image(data.mlx_ptr, "sprites/xpm/kaaris.xpm", &data.width, &data.height);
+	data.textures[1] = mlx_xpm_file_to_image(data.mlx_ptr, "sprites/xpm/harambe.xpm", &data.width, &data.height);
+    data.textures[2] = mlx_xpm_file_to_image(data.mlx_ptr, "sprites/xpm/tile01.xpm", &data.width, &data.height);
+    data.textures[3] = mlx_xpm_file_to_image(data.mlx_ptr, "sprites/xpm/tile02.xpm", &data.width, &data.height);
+    data.textures[4] = mlx_xpm_file_to_image(data.mlx_ptr, "sprites/xpm/tile03.xpm", &data.width, &data.height);
 }
 
 
 void char_to_sprite(t_data data, char *str)
 {
-	int i;
-	
-	i = 0;
-	while (str[i])
-	{
-		int texture_index;  // Index de la texture à utiliser
-		switch (str[i]) {
-			case '1':
-				texture_index = 2;  // Index de la texture pour '1'
-				break;
-			case '0':
-				texture_index = 3;  // Index de la texture pour '0'
-				break;
-			case 'P':
-				texture_index = 0;  // Index de la texture pour 'P'
-				break;
-			case 'C':
-				texture_index = 1;  // Index de la texture pour 'C'
-				break;
-			case 'E':
-				texture_index = 4;  // Index de la texture pour 'E'
-				break;
-			default:
-				// Gérer le cas par défaut (par exemple, caractère inconnu)
-				texture_index = 0;
-				break;
-		}
-		
-		mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[texture_index], data.width, data.height);
-		
-		data.width += 64;
-		i++;
-	}
-	data.height += 64;
+    int x = 0;
+	static int y = 0;
+    int i = 0;
+    int texture_index;
+
+    while (str[i])
+    {
+        switch (str[i]) {
+            case '1':
+                texture_index = 2;
+                break;
+            case '0':
+                texture_index = 3;
+                break;
+            case 'P':
+                texture_index = 0;
+                break;
+            case 'C':
+                texture_index = 1;
+                break;
+            case 'E':
+                texture_index = 4;
+                break;
+        }
+        mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.textures[texture_index], x, y);
+        x += 64;
+        i++;
+    }
+	y += 64;
 }
+
+
 
 
 
