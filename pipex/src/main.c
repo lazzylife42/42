3a1bcaa9914d06e1a412ca7fdfffd10f15d6f81a
Don Lazzylife <88842775+lazzylife42@users.noreply.github.com>
2023-11-04 19:00:20 +0100

    2x execve ok

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,7 +6,7 @@
 /*   By: smonte-e <smonte-e@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 17:33:11 by smonte-e          #+#    #+#             */
-/*   Updated: 2023/11/04 18:05:12 by smonte-e         ###   ########.fr       */
+/*   Updated: 2023/11/04 18:58:55 by smonte-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,45 @@
 
 int main(int argc, char const **argv)
 {
-	pid_t father;
-
+	pid_t	father;
+	int		file0;
+	int		file1;
+	char	*cmd0;
+	char 	*cmd1 = NULL;
+	
 	(void)argc;
-	(void)argv;
+	file0 = open(argv[1], O_RDWR);
+	file1 = open(argv[4], O_RDWR);
+	cmd0 = (char *)malloc((ft_strlen(argv[2]) + 6) * sizeof(char));
+	cmd1 = (char *)malloc((ft_strlen(argv[3]) + 6) * sizeof(char));
+	
+	ft_strcat(cmd0, "/bin/");
+	ft_strcat(cmd1, "/bin/");
+	ft_strcat(cmd0, argv[2]);
+	ft_strcat(cmd1, argv[3]);
+
+	char *const c0[] = {cmd0, NULL};
+	char *const c1[] = {cmd1, NULL};
+	
+	if (file0 == -1 || file1 == -1)
+	{
+		write(2, "Error\nCan't open file(s).\n", 27);
+		exit(EXIT_FAILURE);
+	}
 	father = fork();
 	if (father > 0)
 	{
 		wait(0);
-		ft_printf("Call Me PAPY\n");
+		ft_printf("cmd 1 : '%s'\n", cmd1);
+		execve(cmd1, c1, NULL);
 	}
 	else if (father == 0)
 	{
-		sleep(5);
-		char *const args[] = {"/bin/ls", NULL};
-		execve("/bin/ls", args, NULL);
+		ft_printf("cmd 0 : '%s'\n", cmd0);
+		execve(cmd0, c0, NULL);
 	}
+	free(cmd0);
+	free(cmd1);
 	return 0;
 }
 
