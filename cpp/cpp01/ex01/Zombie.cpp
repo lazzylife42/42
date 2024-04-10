accc11ce27e7dc6dd5c89f1e343c23d9342e2757
Sab <lazzylifemusic@gmail.com>
2024-04-10 19:24:17 +0200

    cpp01_ex01

---
+++ cpp/cpp01/ex01/Zombie.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smonte-e <smonte-e@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 16:29:15 by smonte-e          #+#    #+#             */
+/*   Updated: 2024/04/10 19:06:03 by smonte-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Zombie.hpp"
+
+Zombie::Zombie(void)
+{
+    std::cout << GRN << "Constructor called" << RST << std::endl;
+    return;
+}
+
+Zombie::~Zombie(void)
+{
+    std::cout << RED << "~Destructor called for : " << RST << this->name << RED << " ~"  << RST << std::endl;
+    return;
+}
+
+void Zombie::announce(void)
+{
+    std::cout << this->name << " : BraiiiiiiinnnzzzZ..." << std::endl;
+    return;
+}
+
+void Zombie::nameZombie(std::string name)
+{
+    this->name = name;
+    return;
+}
+
+void Zombie::randomChump(std::string name)
+{
+    Zombie* z = newZombie(name);
+    z->announce();
+    delete z;
+    return;
+}
+
+Zombie* newZombie(std::string name)
+{
+    Zombie* z = new Zombie();
+    z->nameZombie(name);
+    return z;
+}
---
+++ cpp/cpp01/ex01/zombieHorde.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smonte-e <smonte-e@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 19:07:47 by smonte-e          #+#    #+#             */
+/*   Updated: 2024/04/10 19:19:10 by smonte-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Zombie.hpp"
+
+Zombie* zombieHorde(int N, std::string name)
+{
+    Zombie* horde = new Zombie[N];
+
+    for (int i = 0; i < N; i++)
+        horde[i].nameZombie(name);
+
+    return horde;
+}
