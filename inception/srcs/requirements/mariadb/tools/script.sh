8e05b212abf9356bb5c1a540cb49e9ae102c1a36
Sab <lazzylifemusic@gmail.com>
2024-05-10 20:12:18 +0200

    ok sur OSX nsm Debian :'/

--- inception/srcs/requirements/mariadb/tools/script.sh
+++ inception/srcs/requirements/mariadb/tools/script.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-# Charger les variables d'environnement
-# export $(cat .env | xargs)
-
-service mysql start
 # Démarrer le service MySQL
+service mysql start
 
-# # Attendre que MySQL soit prêt
-# until mysqladmin ping &>/dev/null; do
-#     echo "Waiting for MySQL to start..."
-#     sleep 1
-# done
-echo $DB_NAME $DB_USER $DB_PASSWORD
 # Exécuter les commandes SQL
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' ;" >> db1.sql
@@ -20,20 +11,7 @@ echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 service --status-all
 
+# Passer le fichier dans la db
 cat db1.sql | mysql -u root -p 
-
-echo ici3
 service mysql stop
-echo ici4
 mysqld 
-echo ici5
-# tail -f /dev/null
-
-# Arrêter MySQL proprement
-# mysqladmin shutdown
-
-# # Attendre que MySQL s'arrête
-# while pgrep mysqld >/dev/null; do
-#     echo "Waiting for MySQL to stop..."
-#     sleep 1
-# done
--- inception/srcs/requirements/wordpress/tools/wordpress_start.sh
+++ inception/srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -16,7 +16,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	wp core download --allow-root;
 	mv /var/www/wp-config.php /var/www/html/
 	echo "Wordpress: creating users..."
-	wp config create --allow-root --dbpass=${DB_PASSWORD} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbhost=${DB_HOST} --dbprefix=wp_
+	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbhost=${DB_HOST} --dbprefix=wp_
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
 	echo "Wordpress: set up!"
