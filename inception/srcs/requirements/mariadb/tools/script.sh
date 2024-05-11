35cbe8e972908b5f86d1f22d1fdf31063f5a0ddc
Sab <lazzylifemusic@gmail.com>
2024-05-11 18:55:09 +0200

    Inception ok !

--- inception/srcs/requirements/mariadb/tools/script.sh
+++ inception/srcs/requirements/mariadb/tools/script.sh
@@ -2,16 +2,15 @@
 
 # Démarrer le service MySQL
 service mysql start
-
-# Exécuter les commandes SQL
+# Exécuter les commandes SQL en tant qu'utilisateur root
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 service --status-all
-
-# Passer le fichier dans la db
-cat db1.sql | mysql -u root -p 
+# Passer le fichier dans la db en spécifiant le mot de passe
+cat db1.sql | mysql -u root
+# Arrêter le service MySQL
 service mysql stop
-mysqld 
+# Redémarrer MySQL
+mysql
