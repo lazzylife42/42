ade317aed2fd3e15a66cf1a836052b80784b5828
Sab <lazzylifemusic@gmail.com>
2024-05-17 17:05:21 +0200

    ex00 et ex01 ok

---
+++ cpp/cpp_Part.2/cpp06/ex02/Base.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smonte-e <smonte-e@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 16:53:04 by smonte-e          #+#    #+#             */
+/*   Updated: 2024/05/17 16:58:56 by smonte-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_HPP
+# define BASE_HPP
+
+#include <string>
+#include <iostream>
+#include "color.hpp"
+
+class Base
+{
+public:
+    virtual ~Base();
+};
+
+#endif
